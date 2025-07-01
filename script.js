998683d97834d61f5eeafdf4b9bc8a82bd930c1b
milamim99 <camila.muniz06@escola.pr.gov.br>
2025-07-01 11:37:25 -0300

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:Abygail escolhe sua herança
+        alternativas: [
+            "fazenda",
+            "carros de luxo",
+        ]
+    }
+{
+}
+
